e76cd224b8351402387bd15ee52534f0d11f1227
chenrie <calhenrie@icloud.com>
2015-02-03 15:53:33 -0700

    fizzbizz finished

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -10,12 +10,49 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // Insert code here
     
+    for (int i = 1; i < 101; i++) {
+       BOOL buzzed = [self isBuzzed:i];
+       BOOL fizzed = [self isFizzed:i];
+        
+        if (buzzed && !fizzed) {
+            NSLog(@"Buzz");
+        } else if (fizzed && !buzzed) {
+            NSLog(@"Fizz");
+        } else if (fizzed && buzzed){
+            NSLog(@"FizzBuzz");
+        } else {
+            NSLog(@"%d",i);
+        }
+    }
+    
     return YES;
 }
+
+// The method should check to see if the integer is a multiple of 3
+- (BOOL)isBuzzed:(int)integer {
+    if (integer % 3 == 0) {
+        return true;
+    } else if ([[NSString stringWithFormat:@"%d", integer] containsString:@"3"]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+- (BOOL)isFizzed:(int)integer {
+    if (integer % 5 == 0) {
+        return true;
+    } else if ([[NSString stringWithFormat:@"%d", integer] containsString:@"5"]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 
 @end
